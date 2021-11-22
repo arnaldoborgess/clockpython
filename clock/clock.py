a1ca4f6487a6eaee86cb16736d798e6b2b38be34
Arnaldo FIlho <arnalldofilho@gmail.com>
2021-11-22 18:50:19 -0300

    organizando código

--- clock/clock.py
+++ clock/clock.py
@@ -1,26 +1,25 @@
-# -*- coding: utf-8 -*-
 import tkinter as tk
 import datetime
 import locale
 
-locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+locale.setlocale(locale.LC_ALL, 'pt_BR.utf8') # -*- coding: utf-8 -*-
+#locale.setlocale(locale.LC_ALL, 'en_ZW.utf8')
 
 class TelaPrincipal:
     def __init__(self, master):
-        self.nossaTela = master
-        self.lblRelogio = tk.Label(
-            self.nossaTela, font=('Arial', 26), fg='Black')
-        self.lblRelogio.pack(pady=30, padx=30)
+        self.tela = master
+        self.relogiodigital = tk.Label(
+            self.tela, font=('Arial', 26), fg='green', bg = 'Black')
+        self.relogiodigital.pack(pady=30, padx=30)
         self.alteracao()
 
     def alteracao(self):
-        now = datetime.datetime.now()
-        
-        self.lblRelogio['text'] = now.strftime('%A, %d %B %Y\n%H:%M:%S')
-
-        self.nossaTela.after(1000, self.alteracao)
-
+        #now = datetime.datetime.now()
+        agora = datetime.datetime.now()
+        #self.relogiodigital['text'] = agora.strftime('%c')
+        self.relogiodigital['text'] = agora.strftime('%A, %d %B %Y\n%H:%M:%S')
+        self.tela.after(1000, self.alteracao)
 
 janelaRaiz = tk.Tk()
 TelaPrincipal(janelaRaiz)
-janelaRaiz.mainloop()
+janelaRaiz.mainloop() # bloqueia qualquer código até que a janela seja fechada
