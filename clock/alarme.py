d276328b7ae0a568e7e4983602bc41194c98aa8b
Arnaldo Filho <arnalldofilho@gmail.com>
2021-12-09 22:50:33 -0300

    wid

---
+++ clock/alarme.py
@@ -0,0 +1,14 @@
+from time import localtime 
+from pygame import mixer
+
+def alarm():
+    hora = int(input('Digite a hora: '))
+    minuto = int(input('Digite o minuto: '))
+
+    while True:
+        if localtime().tm_hour == hora and localtime().tm_min == minuto:
+            print('HORA DE ACORDAR')
+            mixer.init()
+            mixer.music.load("endereço do arquivo")
+            mixer.music.play()
+        break
